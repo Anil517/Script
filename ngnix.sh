efa567dbc85c5f62f9844b89fbe6a05bf6a071e5
Anil517 <53336408+Anil517@users.noreply.github.com>
2019-07-26 17:26:38 +0530

    Update ngnix.sh

--- ngnix.sh
+++ ngnix.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 sudo apt-get install nginx -y
-sudo -i
 cd  /var/www/html
 sed ' s/nginx/PUC/g' index.nginx-debian.html > index.nginx-debian1.html && mv index.nginx-debian1.html index.nginx-
 debian.html
