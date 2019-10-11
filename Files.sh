5ca1cf7783896644a50bf44c3f8690ef22d841f3
Anil517 <53336408+Anil517@users.noreply.github.com>
2019-10-11 15:06:33 +0530

    Update Files.sh

--- Files.sh
+++ Files.sh
@@ -1,1 +1,11 @@
+#!/bin/bash 
 
+var2="gs://testingdemo/test" 
+
+gsutil ls $var2 | sed "s|$var2/||g"  > DAD
+
+#gsutil ls -r gs://testingdemo/test >>Day 
+
+grep -r ".zip" DAD | sed 's/.zip//g'  > DAD.csv 
+
+#grep -r ".zip" set > asasa.csv
